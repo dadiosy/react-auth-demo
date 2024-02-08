acecab346dfb04cd6cbabc2da1da8ac6c8662e6d
eric <blackicow888@gmail.com>
2024-02-09 00:42:26 +0900

    updated regiser form using formik and yup

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,97 +2,120 @@ import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { registerUser } from "../../actions/authActions";
 import TextFieldGroup from "../common/TextFieldGroup";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 const Register = ({ history }) => {
-
-  const auth = useSelector(state => state.auth)
-  const errors = useSelector(state => state.errors)
-  const dispatch = useDispatch()
-  const [name, setName] = useState('')
-  const [username, setUsername] = useState('')
-  const [email, setEmail] = useState('')
-  const [contact, setContact] = useState('')
-  const [password, setPassword] = useState('')
-  const [password2, setPassword2] = useState('')
+  const auth = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     if (auth.isAuthenticated) {
       history.push("/dashboard");
     }
-  }, [])
-
-  const onSubmit = e => {
-    e.preventDefault();
-    const newUser = {
-      name,
-      username,
-      contact,
-      email,
-      password,
-      password2
-    };
-    dispatch(registerUser(newUser, history))
-  };
+  }, []);
 
   return (
     <div className="register">
       <h2 className="page-header">Register</h2>
-      <form noValidate onSubmit={onSubmit}>
-        <TextFieldGroup
-          label="name"
-          name="name"
-          placeholder="Enter your name"
-          value={name}
-          onChange={e => setName(e.target.value)}
-          error={errors.name}
-        />
-        <TextFieldGroup
-          label="Contact-Number"
-          name="contact"
-          placeholder="Enter your contact number"
-          value={contact}
-          onChange={e => setContact(e.target.value)}
-          error={errors.contact}
-        />
-        <TextFieldGroup
-          label="email"
-          name="email"
-          type="email"
-          placeholder="Enter your email"
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-          error={errors.email}
-        />
-        <TextFieldGroup
-          label="username"
-          name="username"
-          placeholder="Enter your username"
-          value={username}
-          onChange={e => setUsername(e.target.value)}
-          error={errors.name}
-        />
-        <TextFieldGroup
-          label="password"
-          name="password"
-          type="password"
-          placeholder="Enter your password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-          error={errors.password}
-        />
-        <TextFieldGroup
-          label="Confirm Password"
-          name="password2"
-          type="password"
-          placeholder="Confirm password"
-          value={password2}
-          onChange={e => setPassword2(e.target.value)}
-          error={errors.password2}
-        />
-        <input type="submit" className="btn btn-info" value="Register" />
-      </form>
+      <Formik
+        initialValues={{
+          name: "",
+          contact: "",
+          email: "",
+          username: "",
+          password: "",
+          password2: "",
+        }}
+        validationSchema={Yup.object({
+          name: Yup.string().required("Required"),
+          contact: Yup.string()
+            .required("Required")
+            .min(10, "Must be 10 characters")
+            .max(10, "Must be 10 characters"),
+          email: Yup.string()
+            .email("Invalid email address")
+            .required("Required"),
+          username: Yup.string().required("Required"),
+          password: Yup.string().required("Required"),
+          password2: Yup.string().required("Required"),
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          setSubmitting(true);
+          const newUser = {
+            name: values.name,
+            username: values.username,
+            contact: values.contact,
+            email: values.email,
+            password: values.password,
+            password2: values.password2,
+          };
+          dispatch(registerUser(newUser, history));
+          setSubmitting(false);
+        }}
+      >
+        <Form>
+          <label htmlFor="name">name</label>
+          <Field
+            name="name"
+            className="form-control"
+            type="text"
+            placeholder="Enter your name"
+          />
+          <ErrorMessage name="name" />
+          <br />
+          <label htmlFor="contact">contact number</label>
+          <Field
+            name="contact"
+            className="form-control"
+            type="text"
+            placeholder="Enter your contact number"
+          />
+          <ErrorMessage name="contact" />
+          <br />
+          <label htmlFor="email">email</label>
+          <Field
+            name="email"
+            className="form-control"
+            type="email"
+            placeholder="Enter your email"
+          />
+          <ErrorMessage name="username" />
+          <br />
+          <label htmlFor="username">username</label>
+          <Field
+            name="username"
+            className="form-control"
+            type="text"
+            placeholder="Enter your username"
+          />
+          <ErrorMessage name="username" />
+          <br />
+          <label htmlFor="password">password</label>
+          <Field
+            name="password"
+            className="form-control"
+            type="password"
+            placeholder="Enter your password"
+          />
+          <ErrorMessage name="password" />
+          <br />
+          <label htmlFor="password2">confirm password</label>
+          <Field
+            name="password2"
+            className="form-control"
+            type="password"
+            placeholder="Enter your confirm password"
+          />
+          <ErrorMessage name="password2" />
+          <br />
+          <button type="submit" className="btn btn-info">
+            Register
+          </button>
+        </Form>
+      </Formik>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
